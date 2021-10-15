554ea5cd171bb945a9493d21167a7dde0ad8df89
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-10-15 17:18:59 +0700

    problem 2 fixed

---
+++ Final/Assets/Scripts/BallController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    private Rigidbody2D ballRB;
+    private float ballSpeed = 50f;
+
+    private void Start()
+    {
+        ballRB = GetComponent<Rigidbody2D>();
+        MoveBall();
+    }
+
+    private void MoveBall()
+    {
+        float randomAng = Random.Range(0f, 360f);
+        Vector2 randomDir = new Vector2(Mathf.Cos(randomAng), Mathf.Sin(randomAng));
+        ballRB.velocity = randomDir * ballSpeed * Time.fixedDeltaTime;
+    }
+}
