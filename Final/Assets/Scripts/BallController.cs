ba0a7d293851f12bfb6e8c03b6dfef66086ed5ab
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-10-17 21:48:01 +0700

    problem 5 fixed

--- Final/Assets/Scripts/BallController.cs
+++ Final/Assets/Scripts/BallController.cs
@@ -4,14 +4,14 @@ public class BallController : MonoBehaviour
 {
     private Rigidbody2D ballRB;
     Vector2 keyboardInputAxis;
-
+    Vector2 balltoMouseDir;
     [SerializeField] private float ballSpeed = 50f;
     [SerializeField] private MovementType movementType;
 
     private void Start()
     {
         ballRB = GetComponent<Rigidbody2D>();
-       
+        //if we chose to let the ball without control
         if(movementType == MovementType.RANDOM)
         {
             Vector2 ballDir = GetRandomDir();
@@ -22,7 +22,23 @@ public class BallController : MonoBehaviour
 
     private void Update()
     {
+        //if we chose to use keyboard
         keyboardInputAxis = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        //if we chose to use mouse
+        if(movementType == MovementType.MOUSE_CONTROLLED)
+        {
+            Vector3 mousePositionInWorld = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            if(Vector2.Distance(mousePositionInWorld, transform.position) < 0.1f)
+            {
+                balltoMouseDir = Vector2.zero;
+            }
+            else
+            {
+                balltoMouseDir = ((Vector2)(mousePositionInWorld - transform.position)).normalized;
+            }
+        }
+
     }
 
     private void FixedUpdate()
@@ -30,6 +46,10 @@ public class BallController : MonoBehaviour
         if (movementType == MovementType.KEYBOARD_CONTROLLED)
         {
             MoveBall(keyboardInputAxis);
+        }
+        else if(movementType == MovementType.MOUSE_CONTROLLED)
+        {
+            MoveBall(balltoMouseDir);
         }
     }
 
@@ -44,11 +64,11 @@ public class BallController : MonoBehaviour
         Vector2 randomDir = new Vector2(Mathf.Cos(randomAng), Mathf.Sin(randomAng));
         return randomDir;
     }
-
 }
 
 public enum MovementType
 {
     RANDOM,
-    KEYBOARD_CONTROLLED
+    KEYBOARD_CONTROLLED,
+    MOUSE_CONTROLLED
 }
