ca710a0dd099d6e76810f0838a879580d8b7d394
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-10-17 22:43:43 +0700

    problem 7 fixed

---
+++ Final/Assets/Scripts/ScoreController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    #region Singleton
+    private static ScoreController instance = null;
+    public static ScoreController Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType<ScoreController>();
+            }
+            return instance;
+        }
+    }
+    #endregion
+
+    [SerializeField] private Text scoreText;
+    private int score = 0;
+
+    public void IncreaseScore(int scoreCount)
+    {
+        score += scoreCount;
+        scoreText.text = "Score : " + score.ToString();
+    }
+}
