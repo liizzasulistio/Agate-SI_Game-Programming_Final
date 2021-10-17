828fda1fb4a0436e7cccb55fdc0877035eefea39
Lii'zza Sulistio <liizzasulistio@gmail.com>
2021-10-17 21:37:56 +0700

    problem 4 fixed

--- Final/Assets/Scripts/BallController.cs
+++ Final/Assets/Scripts/BallController.cs
@@ -3,13 +3,34 @@
 public class BallController : MonoBehaviour
 {
     private Rigidbody2D ballRB;
+    Vector2 keyboardInputAxis;
+
     [SerializeField] private float ballSpeed = 50f;
+    [SerializeField] private MovementType movementType;
 
     private void Start()
     {
         ballRB = GetComponent<Rigidbody2D>();
-        Vector2 ballDir = GetRandomDir();
-        MoveBall(ballDir);
+       
+        if(movementType == MovementType.RANDOM)
+        {
+            Vector2 ballDir = GetRandomDir();
+            MoveBall(ballDir);
+        }
+        
+    }
+
+    private void Update()
+    {
+        keyboardInputAxis = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+    }
+
+    private void FixedUpdate()
+    {
+        if (movementType == MovementType.KEYBOARD_CONTROLLED)
+        {
+            MoveBall(keyboardInputAxis);
+        }
     }
 
     public void MoveBall(Vector3 randomDir)
@@ -24,4 +45,10 @@ public class BallController : MonoBehaviour
         return randomDir;
     }
 
+}
+
+public enum MovementType
+{
+    RANDOM,
+    KEYBOARD_CONTROLLED
 }
